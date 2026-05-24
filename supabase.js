f4dba38a6402176f2cb563dbdc56a2298568dfbc
je4nc4rlos0903-creator <je4nc4rlos0903@gmail.com>
2026-05-24 02:07:09 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = 'https://ezxedqhjpqrdoseydxdz.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_D9xPlcp27WehxMgZjE_Z4Q_z12AtsdK'
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-)
